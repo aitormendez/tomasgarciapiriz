6ba505b34c3178431ec5041f22645e3c61c02096
aitor <aitor@e451.net>
2021-11-11 07:32:56 +0100

    Ajustes environment

--- site/web/app/themes/sage/resources/scripts/Experience/World/Environment.js
+++ site/web/app/themes/sage/resources/scripts/Experience/World/Environment.js
@@ -16,12 +16,20 @@ export default class Environment
 
     setSunLight()
     {
-        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 10)
         this.sunLight.castShadow = true
-        this.sunLight.shadow.camera.far = 10
+        this.sunLight.shadow.camera.far = 6
+        this.sunLight.shadow.camera.near = 4
+        this.sunLight.shadow.camera.top = 1
+        this.sunLight.shadow.camera.right = 1
+        this.sunLight.shadow.camera.bottom = - 1
+        this.sunLight.shadow.camera.left = - 1
         this.sunLight.shadow.mapSize.set(1024, 1024)
         this.sunLight.shadow.normalBias = 0.05
-        this.sunLight.position.set(3, 3, - 2.25)
+        this.sunLight.position.set(0, 3, - 4)
         this.scene.add(this.sunLight)
+
+        const directionalLightCameraHelper = new THREE.CameraHelper(this.sunLight.shadow.camera)
+        this.scene.add(directionalLightCameraHelper)
     }
 }
