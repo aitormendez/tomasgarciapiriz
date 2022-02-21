42792e5233e76e3d8df3d8fab77411ec5c71775c
aitor <aitor@e451.net>
2022-02-21 11:16:13 +0100

    Academic: archive. Cambiar tamaño titulares

--- site/web/app/themes/sage/resources/views/partials/content-academic.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content-academic.blade.php
@@ -1,6 +1,6 @@
 
 <a href="{{ get_permalink() }}" role="article" id="post-{{ get_the_ID() }}" class="w-full mb-12 md:mb-0 article borde">
-    <h2 class="px-6 mt-6 mb-4 font-bold text-black entry-title">
+    <h2 class="relative px-6 mt-6 mb-4 font-bold text-black entry-title">
         {!! $title !!}
     </h2>
 
